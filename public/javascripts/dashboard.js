b8db8d3a594cce6974d2527375a53073d41defdd
shivashis <shivashispadhi@gmail.com>
2017-10-24 20:48:33 +0530

    Emergency

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -27,11 +27,10 @@ $('#ex1').change(function(){
 	$('.timeDisplay').html($('#ex1').val())
 })
 
-var mymap = L.map('map').setView([51.505, -0.09], 13);
+var mymap = L.map('map').setView([51.505, -0.09], 3);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoicGxhbnQ5OSIsImEiOiJjajh5MzhqdTUyNWxrMzJwOGJ0dWE2NTB0In0.dyjmXnIUF9KYU4ewcTdqcQ', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 10,
-    maxZoom: 10,
+    maxZoom: 16,
     id: 'isawnyu.map-knmctlkh',
     accessToken: 'pk.eyJ1IjoicGxhbnQ5OSIsImEiOiJjajh5MzhqdTUyNWxrMzJwOGJ0dWE2NTB0In0.dyjmXnIUF9KYU4ewcTdqcQ'
 }).addTo(mymap);
@@ -58,3 +57,45 @@ var popup = L.popup()
     .setLatLng([51.5, -0.09])
     .setContent("I am a standalone popup.")
     .openOn(mymap);
+
+$.getJSON("/jsons/map1.geojson", function(json) {
+    console.log(json); // this will show the info it in firebug console
+    var campus = json;
+    x = L.geoJSON(campus, {
+        color: 'grey',
+        fillColor: '#f03',
+        style: function (feature) {
+            return feature.properties && feature.properties.style;
+        },
+
+        onEachFeature: onEachFeature,
+
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, {
+                radius: 8,
+                fillColor: "#ff7800",
+                color: "#000",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            });
+        }
+    });
+    x.addTo(mymap);
+    //remove layer
+    setTimeout(()=>{
+        mymap.removeLayer(x);
+    }, 5000)
+});
+
+
+function onEachFeature(feature, layer) {
+		var popupContent = "<p>I started out as a GeoJSON " +
+				feature.geometry.type + ", but now I'm a Leaflet vector!</p>";
+
+		if (feature.properties && feature.properties.popupContent) {
+			popupContent += feature.properties.popupContent;
+		}
+
+		layer.bindPopup(popupContent);
+	}
